f0e8f0a68550c33c2da302e16ceedb454894af48
Joseph Malloch <joseph.malloch@gmail.com>
2012-04-06 16:40:27 -0400

    Working demonstration.

--- js/main.js
+++ js/main.js
@@ -3,12 +3,15 @@ var devices =   {
                     "children" : []
                 };
 
+var devices2 = new Assoc();
+
 var cluster;
 var diagonal;
 var vis;
 var w = 960;
 var h = 1000;
 var i = 0;
+var timer_is_on = 0;
 
 function setup_display()
 {
@@ -29,7 +32,17 @@ function setup_display()
 
 function update_display()
 {
+    if (!timer_is_on) {
+        timer_is_on = 1;
+        setTimeout("real_update_display()", 500);
+    }
+}
+
+function real_update_display()
+{
     var nodes = cluster.nodes(devices).reverse();
+
+    nodes.forEach(function(d) { d.y = d.depth * 180; });
 
     var node = vis.selectAll("g.node")
         .data(nodes, function(d) { return d.id || (d.id = ++i); });
@@ -39,19 +52,39 @@ function update_display()
         .attr("transform", function(d) { return "translate(" + d.y + "," + d.x + ")"; });
 
     nodeEnter.append("circle")
-        .attr("r", 4.5);
+        .attr("r", 1e-6)
+        .style("fill", function(d) { return d.children ? "lightsteelblue" : "#fff"; });
 
     nodeEnter.append("text")
-        .attr("dx", function(d) { return d.children ? -8 : 8; })
-        .attr("dy", 3)
+        .attr("x", function(d) { return d.children ? -10 : 10; })
+        .attr("dy", ".35em")
         .attr("text-anchor", function(d) { return d.children ? "end" : "start"; })
-        .text(function(d) { return d.name; });
+        .text(function(d) { return d.name; })
+        .style("fill-opacity", 1e-6);
 
     var nodeUpdate = node.transition()
-        .duration(1000)
         .attr("transform", function(d) { return "translate(" + d.y + "," + d.x + ")"; });
 
-    var NodeExit = node.exit().remove();
+    nodeUpdate.select("circle")
+        .attr("r", 4.5)
+        .style("fill", function(d) { return d.children ? "lightsteelblue" : "#fff"; });
+
+    nodeUpdate.select("text")
+        .attr("x", function(d) { return d.children ? -10 : 10; })
+        .attr("dy", ".35em")
+        .attr("text-anchor", function(d) { return d.children ? "end" : "start"; })
+        .text(function(d) { return d.name; })
+        .style("fill-opacity", 1);
+
+    var nodeExit = node.exit().transition()
+        .attr("transform", function(d) { return "translate(" + d.y + "," + d.x + ")"; })
+        .remove();
+
+    nodeExit.select("circle")
+        .attr("r", 1e-6);
+
+    nodeExit.select("text")
+        .style("fill-opacity", 1e-6);
 
     var link = vis.selectAll("path.link")
         .data(cluster.links(nodes), function(d) { return d.target.id; });
@@ -61,10 +94,12 @@ function update_display()
         .attr("d", diagonal);
 
     link.transition()
-        .duration(1000)
         .attr("d", diagonal);
 
-    link.exit().remove();
+    link.exit().transition()
+        .remove();
+
+    timer_is_on = 0;
 }
 
 /* The main program. */
@@ -72,22 +107,18 @@ function main()
 {
     command.register("all_devices", function(cmd, args) {
         for (d in args) {
-            for (i in devices.children) {
-                if (devices.children[i].name == args[d].name)
-                    return;
-            }
             devices.children.push({ "name" : args[d].name,
                                     "children" : [] });
+            devices2[args[d].name] = args;
+            devices2[args[d].name].signals = {};
         }
         update_display();
     });
     command.register("new_device", function(cmd, args) {
-        for (i in devices.children) {
-                if (devices.children[i].name == args.name)
-                    return;
-        }
         devices.children.push({ "name" : args.name,
                                 "children" : [] });
+        devices2[args.name] = args;
+        devices2[args.name].signals = {};
         update_display();
     });
     command.register("del_device", function(cmd, args) {
@@ -106,20 +137,22 @@ function main()
                     break;
                 }
             }
+            devices2[args[d].device_name].signals[args[d].name] = args;
         }
         update_display();
     });
     command.register("new_signal", function(cmd, args) {
         // find device
         var index = -1;
-        for (i in mapping.children[0].children) {
+        for (i in devices.children) {
             if (devices.children[i].name == args.device_name) {
                 devices.children[i].children.push({ "name" : args.name,
                                                     "props" : 0 });
                 update_display();
-                return;
+                break;
             }
         }
+        devices2[args.device_name].signals[args.name] = args;
     });
 //    command.register("del_signal", function(cmd, args) {
 //        mapping.children[1].remove(args.device_name+args.name
